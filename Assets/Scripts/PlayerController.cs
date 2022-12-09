9a22f9b797fdfeb0017f808c0db4794e523ed3b2
Chad Linthicum <portableanswers@gmail.com>
2022-12-09 16:07:36 -0500

    Player can now move vertically.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 30.0f;
     private float xRange = 16.0f;
+    private float zRangeBottom = -4.0f;
+    private float zRangeTop = 16.0f;
 
     public GameObject projectilePrefab;
 
@@ -29,9 +32,22 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        if (transform.position.z < zRangeBottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeBottom);
+        }
+        if (transform.position.z > zRangeTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeTop);
+        }
+
         // Player input
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
