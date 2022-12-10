52fbce3fe5c0c23a744af238e81e254cea1b87f2
Chad Linthicum <portableanswers@gmail.com>
2022-12-10 11:29:32 -0500

    Animals now spawn horizontally and at different intervals. Scenery adjusted to reflect spawn location.

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30.0f;
-    private float bottomBound = -10.0f;
+    private float topBoundary = 30.0f;
+    private float bottomBoundary = -10.0f;
+    private float horizontalBoundary = 22.0f;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If an object goes past the players view in the game, remove that object. 
-        if (transform.position.z > topBound)
+        // If an object goes out of bounds in the game, remove that object. 
+        if (transform.position.z > topBoundary)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < bottomBound)
+        }
+        else if (transform.position.z < bottomBoundary || transform.position.x < -horizontalBoundary || transform.position.x > horizontalBoundary)
         {
             Destroy(gameObject);
             Debug.Log("GAME OVER!");
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,12 +8,16 @@ public class SpawnManager : MonoBehaviour
     private float spawnPosX = 16.0f;
     private float spawnPosZ = 20.0f;
     private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    private float spawnIntervalVertical = 3f;
+    private float spawnIntervalHorizontalLeft = 3.5f;
+    private float spawnIntervalHorizontalRight = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimalVertical", startDelay, spawnIntervalVertical);
+        InvokeRepeating("SpawnRandomAnimalHorizontalLeft", startDelay, spawnIntervalHorizontalLeft);
+        InvokeRepeating("SpawnRandomAnimalHorizontalRight", startDelay, spawnIntervalHorizontalRight);
     }
 
     // Update is called once per frame
@@ -22,11 +26,25 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    void SpawnRandomAnimal()
+    void SpawnRandomAnimalVertical()
     {
         //Randomly generate animal index and spawn position
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(-spawnPosX, spawnPosX), 0, spawnPosZ);
         Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+    void SpawnRandomAnimalHorizontalLeft()
+    {
+        //Randomly generate animal index and spawn position
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(-22, 0, Random.Range(2, 12));
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(0f, 90f, 0f));
+    }
+    void SpawnRandomAnimalHorizontalRight()
+    {
+        //Randomly generate animal index and spawn position
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(22, 0, Random.Range(2, 12));
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(0f, 270f, 0f));
     }
 }
